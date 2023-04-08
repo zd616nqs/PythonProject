c9168da1cc6b0f076f891d953c8368ee215dd21f
牛青山 <niuqingshan@bilibili.com>
2023-04-09 00:22:09 +0800

    更新笔记

--- study/src/02.类型转换计算相关.py
+++ study/src/02.类型转换计算相关.py
@@ -15,6 +15,42 @@ print(hex(ba))  # 十进制转十六进制
 
 
 # -------------------数据类型相关-------------------
+# ------string转int------
 age = "18"
 new_age = int(age)
-print(new_age+2)
+print(new_age+2) # 结果：20
+
+# ------string转int 16进制转10进制------
+cc = "1a2c"
+cd = int(cc, 16) #把字符串1a2c当做十六进制转换成整数
+print(cd)  # 结果：2700
+
+# ------string转int 8进制转10进制------
+cn = "12"
+cm = int(cn, 8) #把字符串12当做八进制转换成整数
+print(cm)  # 结果：10
+
+
+# ------string转float------
+da = "13.14"
+db = float(da)
+dc = 108
+dd = float(dc)
+print(db, dc)  # 结果：13.14  108
+
+# ------int转string------
+ee = 66
+ef = str(ee)
+print(ef,type(ef)) # 66 <class 'str'>
+
+# ------int转bool------
+print(bool(100))  # True
+print(bool(-5))   # True
+print(bool(0))    # False
+
+# ------string转bool------
+# 只有空字符串和None转换为bool才会为False
+print(bool("hello"))  # True
+print(bool("False"))  # True
+print(bool(""), bool(''))       # False False
+print(bool(None))
