0f9d31b69153b125dd39e497a3e801f9a811f10f
牛青山 <niuqingshan@bilibili.com>
2023-04-27 00:26:11 +0800

    更新笔记

--- study/src/17.系统模块_math模块.py
+++ study/src/17.系统模块_math模块.py
@@ -3,6 +3,9 @@ import random
 import datetime
 import time
 import calendar
+import hashlib
+import hmac
+import uuid
 # ----------math系统模块-------
 # 官方文档：https://docs.python.org/zh-cn/3.11/library/math.html
 
@@ -105,3 +108,57 @@ print(calendar.month(2023, 10))
 print(calendar.isleap(2020)) # True
 # 打印xxxx年~yyyy年中间有多少个闰年
 print(calendar.leapdays(2001, 2009)) # 2
+
+
+
+
+# ----------hashlib哈希模块-------
+# 官方文档：https://docs.python.org/zh-cn/3.11/library/hashlib.html
+
+# md5加密
+test_md5 = hashlib.md5()                # 生成一个md5对象
+test_md5.update("牛青山".encode('utf8')) # 加密一段字符串
+print(test_md5.hexdigest())             # d7e31ef085af56d4913e83b70da33353
+
+h0 = hashlib.md5('牛青山'.encode())      
+print(h0.hexdigest())                   # d7e31ef085af56d4913e83b70da33353
+
+# sha加密
+h1 = hashlib.sha1('12345'.encode())
+print(h1.hexdigest()) # 8cb2237d0679ca88db6464eac60da96345513964
+h2 = hashlib.sha224('12345'.encode())
+print(h2.hexdigest()) # a7470858e79c282bc2f6adfd831b132672dfd1224c1e78cbf5bcd057
+h3 = hashlib.sha256('12345'.encode())
+print(h3.hexdigest()) # 5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5
+
+
+
+
+# ----------hmac秘钥模块-------
+# 官方文档：https://docs.python.org/zh-cn/3.11/library/hmac.html
+# hmac也是单向加密
+h4 = hmac.new('12345'.encode(), 'msg_加密盐'.encode(), hashlib.md5)
+print(h4.hexdigest())  # a85e8302af328ed0654bfe40c45d7be7
+h5 = hmac.new('12345'.encode(), 'msg_加密盐'.encode(), hashlib.sha1)
+print(h5.hexdigest())  # 2ccabed4d1f5b4a3cc5da86410abb35072535b48
+print()
+
+# ----------uuid模块-------
+# 用于生成全局唯一值
+# 官方文档：https://docs.python.org/zh-cn/3.11/library/uuid.html
+
+# uuid1 基于mac地址、时间戳、随机数生成唯一uuid，可保证全球范围唯一性
+print(uuid.uuid1()) # 57f429be-e44d-11ed-9188-76a74c640473
+
+# uuid3(namespace,name) 根据命名空间标识符（这是一个UUID）和名称（这是一个字符串）的MD5哈希值，生成一个UUID
+# namespace并不是自己手动置顶的字符串或者其他量，而是在uuid模块内给出的一些纸，比如：uuid.NAMESPACE_DNS uuid.NAMESPACE_OID,这些值本身也是UUID对象，根据一定规则生成
+# 多次运行生成的值是固定的
+print(uuid.uuid3(uuid.NAMESPACE_DNS, '牛牛')) # 0079474e-c7d6-3809-9f41-54e12ba0075b
+
+# uuid4 通过为随机数得到uuid，有一定概率重复
+# 开发过程中使用的最多
+print(uuid.uuid4()) # 291aef17-222a-42db-bfda-95f2c776dd01
+
+# uuid5 跟uuid3类似，只不过采用的散列算法是sha1
+# 多次运行生成的值是固定的
+print(uuid.uuid5(uuid.NAMESPACE_DNS, '牛牛')) # 67c746d8-5f40-5623-a1c1-5573eab4f225
